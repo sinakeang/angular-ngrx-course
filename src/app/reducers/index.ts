36c9df883f3fd41db565b0293659f26e17bdacbf
Sina Keang <sina.keang@innovapost.com>
2018-10-18 17:54:56 -0400

    add ngrx store freeze

--- src/app/reducers/index.ts
+++ src/app/reducers/index.ts
@@ -11,18 +11,13 @@ import {AuthActions, AuthActionTypes} from '../auth/auth.actions';
 import {storeFreeze} from 'ngrx-store-freeze';
 import {routerReducer} from '@ngrx/router-store';
 
-
-export interface AppState {
-
-}
+export interface AppState { }
 
 export const reducers: ActionReducerMap<AppState> = {
   router: routerReducer
 };
 
-
-
-
-
+// MetaReducers are applied after all other reducers have been executed
+// storeFreeze is added to check for unsafe mutation of the store state
 export const metaReducers: MetaReducer<AppState>[] =
   !environment.production ? [storeFreeze] : [];
