79f1256c9d5dd6449e9914738d2c6f46cf381b08
Sina Keang <sina.keang@innovapost.com>
2018-10-18 23:39:51 -0400

    add course effects, reducers, selectors, and resolvers

---
+++ src/app/courses/course.effects.ts
@@ -0,0 +1,19 @@
+import { Injectable } from "@angular/core";
+import { Effect, Actions, ofType } from "@ngrx/effects";
+import { CoursesService } from "./services/courses.service";
+import { CourseRequested, CourseActionTypes, CourseLoaded } from "./course.actions";
+import { mergeMap, map } from "rxjs/operators";
+
+@Injectable()
+export class CourseEffects {
+  
+  @Effect()
+  loadCourse$ = this.actions$
+    .pipe(
+      ofType<CourseRequested>(CourseActionTypes.CourseRequested),
+      mergeMap(action => this.coursesService.findCourseById(action.payload.courseId)),
+      map(course => new CourseLoaded({course})),
+    );
+
+  constructor(private actions$:Actions, private coursesService: CoursesService) { }
+}
--- src/app/courses/services/course.resolver.ts
+++ src/app/courses/services/course.resolver.ts
@@ -1,6 +1,3 @@
-
-
-
 import {Injectable} from "@angular/core";
 import {ActivatedRouteSnapshot, Resolve, RouterStateSnapshot} from "@angular/router";
 import {Course} from "../model/course";
@@ -9,24 +6,37 @@ import {CoursesService} from "./courses.service";
 import {AppState} from "../../reducers";
 import {select, Store} from "@ngrx/store";
 import {filter, first, tap} from "rxjs/operators";
-
-
+import { selectCourseById } from "../course.selectors";
+import { CourseRequested } from "../course.actions";
 
 @Injectable()
 export class CourseResolver implements Resolve<Course> {
-
     constructor(
         private coursesService:CoursesService,
         private store: Store<AppState>) {
 
     }
-
     resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<Course> {
+      const courseId = route.params['id'];
+      // old way
+      //return this.coursesService.findCourseById(courseId);
 
-        const courseId = route.params['id'];
-
-        return this.coursesService.findCourseById(courseId);
+      // new way
+      return this.store
+        .pipe(
+          //look for course in store
+          select(selectCourseById(courseId)),
+          //if course is not in the store
+          //let the store know that a course requested event occur
+          tap(course => {
+            if(!course) {
+              this.store.dispatch(new CourseRequested({courseId}));
+            }
+          }),
+          //filter out the case for when the course is not in the store
+          filter(course => !!course),
+          first()
+        )
     }
-
 }
 
